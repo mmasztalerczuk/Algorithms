3ed106b92bb30743159b1f86145c9848af129313
Mariusz <mmasztalerczuk@gmail.com>
2014-08-31 13:36:40 +0200

    Added myTemplate.cpp

---
+++ myTemplate.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<limits>
+
+#define POS "Possible"
+#define IMPO "Impossible"
+#define MAXN 11
+#define INF std::numeric_limits<int>::max()/2
+#define FOR(x,y) for(int x=0;x<y;x++)
+#define FOR_S(x,a,y) for(int x=a;x<y;x++)
+using namespace std;
+
+void Floyd(int tab[][MAXN], int size)
+{
+	FOR(i,size)
+	 FOR(j,size)
+          FOR(k,size)
+	  {
+		if(tab[j][k] > tab[j][i] + tab[i][k])
+		{
+			tab[j][k] = tab[j][i] + tab[i][k];
+		}
+          }
+}
+
+vector<vector<int> > AllSets(int e)
+{
+	vector<vector<int> > ans;
+	FOR(i, (1 << e))
+	{
+		vector<int> a;		
+		FOR(j,(e+1))
+		{
+			if ((1 << j) & i)
+			{
+				a.push_back(j);
+				cout << "     " << j << endl;
+			}
+		}
+		if (a.size() > 0)
+			ans.push_back(a);
+	}
+	return ans;
+}
